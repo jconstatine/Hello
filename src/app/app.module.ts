5da2e52d1457940a6701e6e3b5274655d67fa555
Md.akram <akramuddin.text@gmail.com>
2022-08-18 22:41:50 +0530

    library added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule      // this module is used to call http client api url in service
   ],
   providers: [],
   bootstrap: [AppComponent]
